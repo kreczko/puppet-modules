a735043974392a11c08921a2ff371dcb1d28b2ef
kreczko <lkreczko@gmail.com>
2013-05-30 14:38:53 +0100

    adding message of the day

---
+++ motd/manifests/init.pp
@@ -0,0 +1,24 @@
+# Class: motd
+#
+# This module manages the /etc/motd file using a template
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#  include motd
+#
+# [Remember: No empty lines between comments and class definition]
+#DICE to be replaced with cluster
+class motd {
+  if $kernel == "Linux" {
+    file { '/etc/motd':
+      ensure  => file,
+      backup  => false,
+      content => "puppet://modules/motd/motd.DICE",
+    }
+  }
+}
