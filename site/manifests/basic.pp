84992435c45458bb9cf9b0ebbba9859ea5f392fa
kreczko <lkreczko@gmail.com>
2013-05-31 17:22:25 +0100

    adding first version of site module

---
+++ site/manifests/basic.pp
@@ -0,0 +1,22 @@
+class site::basic {
+  package {'puppet':
+    ensure => installed,
+  }
+  
+  service {'puppet':
+    ensure => "running",
+    enable => true,
+  }
+  
+  file {'/etc/puppet/puppet.conf':
+    notify  => Service["puppet"],
+    mode    => 644,
+    owner   => "root",
+    group   => "root",
+    ensure => "present",
+    source => "puppet:///modules/site/puppet.conf",
+    require => Package["puppet"],
+  }
+  
+  
+}
